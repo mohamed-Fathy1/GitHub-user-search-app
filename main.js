484505a75dacac6e43417fa897929c634f2dfb4a
mohamed-Fathy1 <mohamedfathysaied18@gmail.com>
2024-02-12 10:59:03 +0200

    Add themes

--- main.js
+++ main.js
@@ -14,30 +14,45 @@ document.addEventListener('DOMContentLoaded', () => {
     document.getElementById('search').addEventListener('click', async () => {
         const username = document.getElementById('username').value
         const userData = await getUserData(username)
-        .then(data => {
-            document.getElementById('name').innerText = data.name
-            document.getElementById('handle').innerText = `@${data.login}`
-            document.getElementById('avatar').src = data.avatar_url
-            document.getElementById('bio').innerText = data.bio ? 'This person is a developer' : 'This profile has no bio'
-            document.getElementById('followers').innerText = data.followers
-            document.getElementById('following').innerText = data.following
-            document.getElementById('repos').innerText = data.public_repos
+            .then(data => {
+                document.getElementById('name').innerText = data.name
+                document.getElementById('handle').innerText = `@${data.login}`
+                document.getElementById('avatar').src = data.avatar_url
+                document.getElementById('bio').innerText = data.bio ? 'This person is a developer' : 'This profile has no bio'
+                document.getElementById('followers').innerText = data.followers
+                document.getElementById('following').innerText = data.following
+                document.getElementById('repos').innerText = data.public_repos
 
-            userDetail.forEach(detail => {
-                if (data[detail] === null || data[detail] === '') {
-                    document.getElementById(detail).innerText = 'Not Available'
-                    document.getElementById(detail).parentElement.style.color = '#8E94A4'
-                } else {
-                    document.getElementById(detail).innerText = data[detail]
-                    document.getElementById(detail).parentElement.style.color = '#FFFFFD'
-                }
+                userDetail.forEach(detail => {
+                    if (data[detail] === null || data[detail] === '') {
+                        document.getElementById(detail).innerText = 'Not Available'
+                        document.getElementById(detail).parentElement.style.color = '#8E94A4'
+                    } else {
+                        document.getElementById(detail).innerText = data[detail]
+                        document.getElementById(detail).parentElement.style.color = '#FFFFFD'
+                    }
+                })
+                // years since account was created
+                // example: joind 1 year ago
+                const joined = new Date(data.created_at)
+                const today = new Date()
+                const years = today.getFullYear() - joined.getFullYear()
+                document.getElementById('joined').innerText = `Joined ${years} years ago`
             })
-            // years since account was created
-            // example: joind 1 year ago
-            const joined = new Date(data.created_at)
-            const today = new Date()
-            const years = today.getFullYear() - joined.getFullYear()
-            document.getElementById('joined').innerText = `Joined ${years} years ago`
-        })
     })
+
+
+    const darkModeToggle = document.getElementById('theme-switcher');
+    const body = document.body;
+    const darkMode = localStorage.getItem('darkMode') || false;
+
+    darkModeToggle.addEventListener('click', () => {
+        body.classList.toggle('dark-theme');
+        localStorage.setItem('darkMode', !darkMode);
+    });
+
+    // Check if dark mode preference is stored in localStorage
+    if (darkMode) {
+        body.classList.toggle('dark-theme', JSON.parse(darkMode));
+    }
 })
